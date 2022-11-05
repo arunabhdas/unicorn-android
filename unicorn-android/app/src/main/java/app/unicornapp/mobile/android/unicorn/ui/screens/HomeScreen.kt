0d1a7b3efbb9cf5be0e844af0a388f6f4035b3d9
Arunabh Das <arunabhdas@gmail.com>
2022-11-05 17:05:05 -0400

    Improved layout of HomeScreen

--- unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/ui/screens/HomeScreen.kt
+++ unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/ui/screens/HomeScreen.kt
@@ -2,13 +2,16 @@ package app.unicornapp.mobile.android.unicorn.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -33,15 +36,31 @@ fun HomeScreen(
             contentScale = ContentScale.FillBounds,
             modifier = Modifier.matchParentSize()
         )
-        Text(
-            modifier = Modifier.clickable {
-                navController.navigate(route = Screen.HomeDetailScreen.route)
-            },
-            text = "Go To Detail",
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = MaterialTheme.typography.titleSmall.fontSize,
-            fontWeight = FontWeight.Bold
-        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.clickable {
+                    navController.navigate(route = Screen.HomeDetailScreen.route)
+                },
+                text = "Welcome to Unicorn",
+                color = Color.White,
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier.clickable {
+                    navController.navigate(route = Screen.HomeDetailScreen.route)
+                },
+                text = "Brace for Liftoff",
+                color = Color.White,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
     }
 
 }
