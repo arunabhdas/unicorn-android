dfa63b37fc150751e6cca6c19936a9eb8af3b08b
Arunabh Das <arunabhdas@gmail.com>
2022-11-19 02:31:20 -0500

    Added CustomAppBar preview

--- unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/ui/navigation/CustomAppBar.kt
+++ unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/ui/navigation/CustomAppBar.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.unicornapp.mobile.android.unicorn.R
 
@@ -53,4 +54,12 @@ fun CustomAppBar(
             }
         )
     }
+}
+
+@Preview
+@Composable
+fun CustomAppBarPreview() {
+   CustomAppBar(
+       {}
+   )
 }
